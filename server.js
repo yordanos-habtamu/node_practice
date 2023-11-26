98a5cdcd7c671b5308b640e9a4152a728bb20167
yordanos habtamu <jordrichh7@gmail.com>
2023-11-26 09:22:10 +0300

    my first and last commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express=require('express')
+const app=express()
+const jwt=require('jsonwebtoken')
+app.use(express.json())
+const posts=[
+    {
+   user:"kim",
+   post:"post1"
+    },
+    {
+        user:"kill",
+        post:"post2"
+    },
+]
+app.get('/posts',(req,res)=>{
+ res.json(posts)
+})
+app.post('/login',(req,res)=>{
+    const username=req.body.user
+    const user={user:username}
+  const accesstoken=  jwt.sign(user,process.env.ACCESS_SECRET_TOKEN)
+  res.json({accesstoken:accesstoken})
+})
+function authenticateToken(req,res,next){
+    const authHeader=req.headers[authorization]
+    const token=authHeader && authHeader.split(' ')[1]
+    if (token==null) return res.sendStatus(401)
+   jwt.verify(token,process.env.ACCESS_SECRET_TOKEN,(err,user)=>{
+   if(err) return res.sendStatus(403)
+   req.user=user
+   next()
+})
+}
+app.listen(3000)
